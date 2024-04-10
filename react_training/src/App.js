d779b9886464f93f5afe913aca129867478e0c95
PrathameshMahadik <prathamesh.mahadik@successive.tech>
2024-04-10 16:26:38 +0530

    Day3 Updated

--- react_training/src/App.js
+++ react_training/src/App.js
@@ -1,8 +1,12 @@
 import Index from "./day1/Index";
+import Day2 from "./Day2/Day2";
+import Day3 from "./Day3/Day3";
 function App() {
   return (
     <div className="App">
-     <Index />
+      <Index />
+      <Day2 />
+      <Day3 />
     </div>
   );
 }
--- react_training/src/day1/Task2/UserCard.jsx
+++ react_training/src/day1/Task2/UserCard.jsx
@@ -1,16 +1,19 @@
 /* 2.Create a functional component named UserCard that accepts the following props: name, email, and avatarURL.
 Use the props to display the user's name, email, and an image (avatar) in the UserCard component.
 Create multiple instances of the UserCard component with different user data. */
+import "./index.css";
 const UserCard = (props) => {
   const { userData } = props;
   return (
     <div>
-      {userData.map((x,index) => {
+      {userData.map((x, index) => {
         return (
-          <section key= {`name_${index}`}>
-            <img src={x.avatarURL} alt=""/>
-            <br />{x.name}
-            <br />{x.email}
+          <section key={`name_${index}`} className="card">
+            <img src={x.avatarURL} alt="" className="avatar" />
+            <div className="user-info">
+              <h2>Username :{x.name}</h2>
+              <p>Email ID :{x.email}</p>
+            </div>
           </section>
         );
       })}
